fc77fe4045d6e72249e360005833849e2e4df57e
Maximiliano Burgos <maxib2007@gmail.com>
2019-05-06 16:58:03 -0300

    Implementamos los controles del mapa

--- app/src/main/java/com/maxwell/finder/PartialMapActivity.java
+++ app/src/main/java/com/maxwell/finder/PartialMapActivity.java
@@ -1,21 +1,15 @@
 package com.maxwell.finder;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PartialMapActivity extends BaseActivity implements OnMapReadyCallback {
+public class PartialMapActivity extends BaseActivity implements OnMapReadyCallback, View.OnClickListener {
     private GoogleMap mMap;
 
     @Override
@@ -26,10 +20,48 @@ public class PartialMapActivity extends BaseActivity implements OnMapReadyCallba
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button scrollUp = findViewById(R.id.scrollUp);
+        Button scrollDown = findViewById(R.id.scrollDown);
+        Button scrollRight = findViewById(R.id.scrollRight);
+        Button scrollLeft = findViewById(R.id.scrollLeft);
+        Button zoomIn = findViewById(R.id.zoomIn);
+        Button zoomOut = findViewById(R.id.zoomOut);
+
+        scrollUp.setOnClickListener(this);
+        scrollDown.setOnClickListener(this);
+        scrollRight.setOnClickListener(this);
+        scrollLeft.setOnClickListener(this);
+        zoomIn.setOnClickListener(this);
+        zoomOut.setOnClickListener(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.scrollUp:
+                mMap.animateCamera(CameraUpdateFactory.scrollBy(0, -100));
+                break;
+            case R.id.scrollDown:
+                mMap.animateCamera(CameraUpdateFactory.scrollBy(0, 100));
+                break;
+            case R.id.scrollRight:
+                mMap.animateCamera(CameraUpdateFactory.scrollBy(100, 0));
+                break;
+            case R.id.scrollLeft:
+                mMap.animateCamera(CameraUpdateFactory.scrollBy(-100, 0));
+                break;
+            case R.id.zoomIn:
+                mMap.moveCamera(CameraUpdateFactory.zoomIn());
+                break;
+            case R.id.zoomOut:
+                mMap.moveCamera(CameraUpdateFactory.zoomOut());
+                break;
+        }
     }
 }
